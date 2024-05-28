474cb3be1342845b7bdf8f0daaf5c19f35a3beda
WonJun-62 <132991365+WonJun-62@users.noreply.github.com>
2024-05-28 18:57:12 +0900

    아영님 코드 업데이트

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-
     $('body').addClass('fade-in');
 
     function goToPage(url) {
         $('body').addClass('fade-out'); 
         setTimeout(function() {
-        window.location.href = url; 
+            window.location.href = url; 
         }, 500); 
     }
 
@@ -16,4 +15,40 @@ $(document).ready(function() {
     $('.rules-button').click(function() {
         goToPage('rule.html');
     });
+
+    // 추가된 부분
+
+    var video = $('video');
+    var pageWrapper = $('#page-wrapper');
+    var skipButton = $('#skip-button');
+    var backgroundMusic = $('#background-music')[0];
+    var musicButton = $('#music-button');
+    var isMusicPlaying = false;
+    var musicButtonImage = $('#music-button img');
+
+    pageWrapper.hide();
+
+    video.on('ended', function() {
+        pageWrapper.fadeIn(1000); 
+        video.remove(); 
+        skipButton.remove(); 
+    });
+
+    skipButton.click(function() {
+        pageWrapper.fadeIn(1000); 
+        video.remove(); 
+        skipButton.remove(); 
+    });
+
+    musicButton.click(function() {
+        if (isMusicPlaying) {
+            backgroundMusic.pause();
+            musicButtonImage.attr('src', 'audio_off.png');
+        } else {
+            backgroundMusic.play();
+            musicButtonImage.attr('src', 'audio_on.png');
+        }
+        isMusicPlaying = !isMusicPlaying;
+    });
+
 });
